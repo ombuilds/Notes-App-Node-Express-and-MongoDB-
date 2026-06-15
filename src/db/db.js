61a9310660e6493f020a4fb90ea2e64ff8b13077
om ingle <155917128+OmIngle007@users.noreply.github.com>
2026-06-15 19:54:09 +0530

    Move MongoDB URI to environment variables

--- src/db/db.js
+++ src/db/db.js
@@ -5,14 +5,19 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 console.log('DNS Servers:', dns.getServers());
 
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 
-async function connectDB(){
-    
-    await mongoose.connect("mongodb+srv://new_user:2WFZHxn6ZIh6pm3O@cluster0.uttsfm5.mongodb.net/halley")
 
-    console.log("connected to DB")
-}
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
 
 module.exports=connectDB;
 
